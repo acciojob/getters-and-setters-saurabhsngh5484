b489ed6c6ddcaefaacdec13fd2f71edb0e7002af
saurabh singh <86177535+saurabhsngh5484@users.noreply.github.com>
2023-05-13 21:50:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-//complete this code
-class Person {}
-
-class Student extends Person {}
-
-class Teacher extends Person {}
-
-// Do not change the code below this line
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+ 
+  get name() {
+    return this._name;
+  }
+ 
+  /**
+   * @param {any} age
+   */
+  set age(age) {
+    this._age = age;
+  }
+}
+ 
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+ 
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
+ 
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
